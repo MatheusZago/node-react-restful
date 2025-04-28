6372f07d28b34e7b65b09c2e8f98703ea73f5108
Matheus <matheuszago134@gmail.com>
2025-04-28 10:32:53 -0300

    Add database connection with knex

--- backend/server.js
+++ backend/server.js
@@ -1,7 +1,10 @@
 const express = require('express');
+const db = require('./database')
 
 const app = express();
 const PORT = 3000;
+
+app.use(express.json()) 
 
 app.get('/', (req, res) => {
   res.send('The server is working!');
